5a5cee6ad721d40696997cd46e688883396a3e37
Shi Sha <emailtotheowner@gmail.com>
2020-08-08 16:13:50 -0400

    MongoDB database set

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
+// Importing dependencies
 const express = require("express");
 const morgan = require("morgan");
 
+// Initializing express (app)
 const app = express();
 
+// Creating port for localhost and future deployment
 const PORT = process.env.PORT || 5000;
 
+// Importing to use dotenv
 require("dotenv").config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//HTTP Request Logger
 app.use(morgan("tiny"));
 
+// Creating a simple route for testing
 app.get("/", (req, res) => {
-  console.log("This is working!!!!!!");
+  res.send("API is working!!!!!!");
 });
 
+// Initializing localhost server to run the app in development environment
 app.listen(PORT, () => {
   console.log(`App is running on: http://localhost:${PORT}`);
 });
