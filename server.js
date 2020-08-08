1191d162cb754a5246c914579b09e93b85a0e7bf
Shi Sha <emailtotheowner@gmail.com>
2020-08-08 11:23:51 -0400

    gitignore file created and more dependencies installed

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   console.log("This is working!!!!!!");
